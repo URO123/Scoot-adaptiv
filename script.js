a58243f154faf1eaeadcacfa362b6639a3f86b0c
unknown <uro123uzb@gmail.com>
2024-01-25 13:01:52 +0500

    add about

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-elBtn = document.querySelector('.burger-btn');
-elContent = document.querySelector('.site-header__list');
+elBtns = document.querySelector('.burger-btn');
+elContents = document.querySelector('.site-header__list');
+elBtns.addEventListener('click', function() {
+    elContents.classList.toggle('open');
+});
+
+
+elBtn = document.querySelector('.button123');
+elContent = document.querySelector('.site-item-bottom');
 elBtn.addEventListener('click', function() {
-    elContent.classList.toggle('open');
+    elContent.classList.toggle('opens');
+    elBtn.classList.toggle('rotate');
+});
+
+
+elBtn1 = document.querySelector('.button1231');
+elContent1 = document.querySelector('.site-item-bottom1');
+elBtn1.addEventListener('click', function() {
+    elContent1.classList.toggle('opens');
+    elBtn1.classList.toggle('rotate');
+});
+
+
+elBtn2 = document.querySelector('.button1232');
+elContent2 = document.querySelector('.site-item-bottom2');
+elBtn2.addEventListener('click', function() {
+    elContent2.classList.toggle('opens');
+    elBtn2.classList.toggle('rotate');
+});
+
+
+elBtn3 = document.querySelector('.button1233');
+elContent3 = document.querySelector('.site-item-bottom3');
+elBtn3.addEventListener('click', function() {
+    elContent3.classList.toggle('opens');
+    elBtn3.classList.toggle('rotate');
+});
+
+
+elBtn4 = document.querySelector('.button1234');
+elContent4 = document.querySelector('.site-item-bottom4');
+elBtn4.addEventListener('click', function() {
+    elContent4.classList.toggle('opens');
+    elBtn4.classList.toggle('rotate');
+});
+
+
+elBtn5 = document.querySelector('.button1235');
+elContent5 = document.querySelector('.site-item-bottom5');
+elBtn5.addEventListener('click', function() {
+    elContent5.classList.toggle('opens');
+    elBtn5.classList.toggle('rotate');
 });
